9443b61c39622490988e45d32a78a6bff91f6491
thinktainer <thinktainer@me.com>
2017-04-28 14:24:47 +0100

    extracts indexes param to env

--- mongo/index/init.sh
+++ mongo/index/init.sh
@@ -3,12 +3,5 @@ set -e
 mongo ${MONGO_HOST:?}:${MONGO_PORT:?}/${USER_DB} --authenticationDatabase ${ADMIN_DB:?} --password ${PASSWORD:?} --username ${USERNAME:?} --eval "
 db.runCommand({
   createIndexes: \"${COLLECTION:?}\",
-  indexes: [{
-    key: {${INDEX_FIELD:?}: 1},
-    unique: ${INDEX_UNIQUE:-false},
-    name: \"${INDEX_NAME:?}\",
-    partialFilterExpression: {
-      ${FILTER_EXPRESSION}
-    },
-  }]
+  indexes: ${INDEXES:?}
 })"
