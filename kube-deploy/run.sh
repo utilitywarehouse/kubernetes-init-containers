78d0eda4bcf6759146e3ac64dbedc6b51d753121
Martin Schinz <me@thinktainer.com>
2017-09-21 11:49:40 +0100

    add kube-deploy

---
+++ kube-deploy/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -u
+set -e
+set -x
+
+API_ENDPOINT="${API_ENDPOINT:-https://elb.master.k8s.$KUBE_ENV.uw.systems/apis/extensions/v1beta1/namespaces/$NAMESPACE/deployments/$DEPLOYMENT}"
+
+curl -f -X PATCH -k \
+	-d "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"$CONTAINER\",\"image\":\"$IMAGE:$TAG\"}]}}}}" \
+	-H "Content-Type: application/strategic-merge-patch+json" \
+	-H "Authorization: Bearer $K8S_DEV_TOKEN" \
+	"$API_ENDPOINT"
+
