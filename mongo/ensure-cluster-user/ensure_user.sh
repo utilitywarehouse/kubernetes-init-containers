1929fe60f18bd62fc688a97a26eb87746cb30841
Daniel Copaciu <dani.copaciu@gmail.com>
2017-12-04 12:15:25 +0000

    Connect directly to master

--- mongo/ensure-cluster-user/ensure_user.sh
+++ mongo/ensure-cluster-user/ensure_user.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-nodes=$(echo $MONGO_NODES | tr "," "\n")
-masterMarker='"ismaster" : true'
+mongoURL="mongodb://${SERVICE_NAME}/${MONGO_DB}?replicaSet=${REPLICA_SET}"
 
-for node in $nodes
-do
-    checkMaster="mongo $node --username ${ADMIN_USERNAME:?} --password ${ADMIN_PASSWORD:?} --authenticationDatabase ${ADMIN_DB:?} --quiet --eval 'rs.isMaster()'"
-    isMaster=$(eval "$checkMaster")
+checkUserExists="mongo $mongoURL --username ${ADMIN_USERNAME:?} --password ${ADMIN_PASSWORD:?} --authenticationDatabase ${ADMIN_DB:?} --quiet --eval 'db.getSiblingDB(\"${MONGO_DB:?}\").getUser(\"${MONGO_USERNAME:?}\");'"
+userExists=$(eval "$checkUserExists")
 
-    if [[ $isMaster = *$masterMarker* ]]; then
-        checkUserExists="mongo $node --username ${ADMIN_USERNAME:?} --password ${ADMIN_PASSWORD:?} --authenticationDatabase ${ADMIN_DB:?} --quiet --eval 'db.getSiblingDB(\"${MONGO_DB:?}\").getUser(\"${MONGO_USERNAME:?}\");'"
-        userExists=$(eval "$checkUserExists")
-
-        if [[ $(eval "echo -n \"$userExists\"") = "null" ]]; then
-            echo "creating user"
-            createUser="mongo $node --username ${ADMIN_USERNAME:?} --password ${ADMIN_PASSWORD:?} --authenticationDatabase ${ADMIN_DB:?} --quiet --eval 'db.getSiblingDB(\"${MONGO_DB:?}\").createUser({user: \"${MONGO_USERNAME:?}\", pwd: \"${MONGO_PASSWORD:?}\", roles: [{ role: \"${ROLE:?}\", db: \"${MONGO_DB:?}\"}]})'"
-            result=$(eval "$createUser")
-            echo $result
-        fi
-    fi
-done
+if [[ $(eval "echo -n \"$userExists\"") = "null" ]]; then
+    createUser="mongo $mongoURL --username ${ADMIN_USERNAME:?} --password ${ADMIN_PASSWORD:?} --authenticationDatabase ${ADMIN_DB:?} --quiet --eval 'db.getSiblingDB(\"${MONGO_DB:?}\").createUser({user: \"${MONGO_USERNAME:?}\", pwd: \"${MONGO_PASSWORD:?}\", roles: [{ role: \"${ROLE:?}\", db: \"${MONGO_DB:?}\"}]})'"
+    result=$(eval "$createUser")
+    echo $result
+fi
