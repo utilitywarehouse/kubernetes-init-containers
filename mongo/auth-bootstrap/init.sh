20728d0243b77a970acabfc30907b0ee731e8a21
Daniel Copaciu <dani.copaciu@gmail.com>
2018-02-16 10:50:50 +0200

    Mongo bootstrap with users

---
+++ mongo/auth-bootstrap/init.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+[ -z "$ADMIN_PASSWORD" ] && echo "Error not set ADMIN_PASSWORD" && exit 1
+[ -z "$EXPORTER_PASSWORD" ] && echo "Error not set EXPORTER_PASSWORD" && exit 1
+[ -z "$MONGOLIZER_PASSWORD" ] && echo "Error not set MONGOLIZER_PASSWORD" && exit 1
+
+if [ -f ${DB_ROOT}/mongod.lock ]; then
+    exit 0
+fi
+
+gosu root chown -R root: ${DB_ROOT}
+
+gosu root mongod  --fork --logpath ${DB_ROOT}/init-admin.log
+if [ $? -ne 0 ]; then
+    cat ${DB_ROOT}/init-admin.log
+    exit 1
+fi
+
+set -e
+sleep 30
+
+echo "creating user ${ADMIN_USERNAME}"
+mongo --quiet --eval "
+db.getSiblingDB(\"admin\").createUser({
+  user: \"${ADMIN_USERNAME:?}\",
+  pwd: \"${ADMIN_PASSWORD:?}\",
+  roles: [{
+	role: \"root\",
+	db: \"admin\"
+  }]
+});
+"
+
+mongo --quiet --eval "
+db.getSiblingDB(\"admin\").createUser({
+    user: \"${EXPORTER_USERNAME:?}\",
+    pwd: \"${EXPORTER_PASSWORD:?}\",
+    roles: [
+        { role: \"clusterMonitor\", db: \"admin\" },
+        { role: \"read\", db: \"local\" }
+    ]
+});
+"
+
+mongo --quiet --eval "
+db.getSiblingDB(\"admin\").createUser({
+    user: \"${MONGOLIZER_USERNAME:?}\",
+    pwd: \"${MONGOLIZER_PASSWORD:?}\",
+    roles: [\"readWriteAnyDatabase\"]
+});"
+
+mongod --shutdown
