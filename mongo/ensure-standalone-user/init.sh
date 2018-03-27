ff67d613c9eaff44da2171249753bfc8b73321bf
Zoltan Bodor <zbodor@utilitywarehouse.co.uk>
2018-03-27 12:45:33 +0100

    Make role an env var & have default mongo port

--- mongo/ensure-standalone-user/init.sh
+++ mongo/ensure-standalone-user/init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-mongo ${MONGO_HOST:?}:${MONGO_PORT:?}/${USER_DB} --authenticationDatabase ${ADMIN_DB:?} --password ${ADMIN_PASSWORD:?} --username ${ADMIN_USERNAME:?} --eval "
+mongo ${MONGO_HOST:?}:${MONGO_PORT:=27017}/${USER_DB} --authenticationDatabase ${ADMIN_DB:?} --password ${ADMIN_PASSWORD:?} --username ${ADMIN_USERNAME:?} --eval "
 db.createUser({
   user: \"${USERNAME:?}\",
   pwd: \"${PASSWORD:?}\",
     roles: [{
-      role: \"readWrite\",
+      role: \"${ROLE:?}\",
       db: \"${USER_DB:?}\"
     }]
 })
