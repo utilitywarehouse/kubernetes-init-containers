b2c211022250339f76657739d52058e54112c59c
Mike Kaperys <mike@kaperys.io>
2019-07-30 09:35:25 +0100

    fix mongo sharded replication auth

--- mongo/shard_bootstrap/init.sh
+++ mongo/shard_bootstrap/init.sh
@@ -9,7 +9,7 @@
 [[ `hostname` =~ -([0-9]+)$ ]] || exit 1
 ordinal=${BASH_REMATCH[1]}
 
-# Execute user creation and replication setup on the first node only
+# Execute user creation and replication setup on the first node only.
 if [[ $ordinal -ne 0 ]]; then
     exit 0
 fi
@@ -23,7 +23,7 @@ fi
 set -e
 sleep 30
 
-# check if replication is initialized 
+# We only want to continue if a replica set configuration does not exist.
 res="$(mongo --quiet --eval='printjson(rs.status())')"
 if [[ $res != *"NotYetInitialized"* ]]; then
     exit 0
@@ -35,12 +35,9 @@ echo "starting bootstrap"
 echo "${REPL_SET}"
 echo "${REPLICATION_NODES}"
 mongo --quiet --eval "
-rs.initiate( {
-   _id : \"${REPL_SET}\",
-   members: [ { _id : 0, host : \"${name}\" } ]
-})"
+rs.initiate( { _id : \"${REPL_SET}\", members: [ { _id : 0, host : \"${name}\" } ] })"
 
-# wait for mongodb to understand that it is master
+# Wait for mongodb to understand that it is the current master.
 sleep 10
 
 echo "creating user ${ADMIN_USERNAME}"
@@ -52,8 +49,7 @@ db.getSiblingDB(\"admin\").createUser({
 	role: \"root\",
 	db: \"admin\"
   }]
-});
-" 
+});"
 
 mongo --quiet --eval "
 db.getSiblingDB(\"admin\").createUser({
@@ -63,8 +59,7 @@ db.getSiblingDB(\"admin\").createUser({
         { role: \"clusterMonitor\", db: \"admin\" },
         { role: \"read\", db: \"local\" }
     ]
-});
-"
+});"
 
 mongo --quiet --eval "
 db.getSiblingDB(\"admin\").createUser({
@@ -73,7 +68,7 @@ db.getSiblingDB(\"admin\").createUser({
     roles: [{ role: \"backup\", db:\"admin\"}]
 });"
 
-
+# Add each node from $REPLICATION_NODES to the cluster.
 nodes=$(echo $REPLICATION_NODES | tr "," "\n")
 counter=1
 for node in $nodes
@@ -81,7 +76,7 @@ do
     echo "adding replica ${node}, ${counter}"
     while true
     do
-        script="mongo --quiet --eval 'rs.add({_id: ${counter}, host:\"${node}\", priority: 0.99})'"
+        script="mongo --quiet --eval -u ${ADMIN_USERNAME} -p ${ADMIN_PASSWORD} 'rs.add({_id: ${counter}, host:\"${node}\", priority: 0.99})'"
         out=$(eval "$script")
         echo $out
         if [[ $out != *"NodeNotFound"* ]]; then
@@ -93,6 +88,7 @@ do
     counter=$[$counter +1]
 done
 
+# Reconfigure the cluster node priorities.
 NEWLINE=$'\n'
 script="cfg = rs.conf();$NEWLINE"
 script="$script cfg.members[0].priority = 1;$NEWLINE"
@@ -104,6 +100,6 @@ do
     counter=$[$counter +1]
 done 
 script="$script rs.reconfig(cfg);$NEWLINE"
-mongo --quiet --eval "$script"
+mongo --quiet --eval -u ${ADMIN_USERNAME} -p ${ADMIN_PASSWORD} "$script"
 
 mongod --shutdown --dbpath ${DB_ROOT} 
