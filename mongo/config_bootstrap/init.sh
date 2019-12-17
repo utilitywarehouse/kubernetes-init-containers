75cb749e2e88322b7a9bcfc7d5b3a2292e866177
Mike Kaperys <mike@kaperys.io>
2019-12-17 14:25:58 +0000

    more experimenting

--- mongo/config_bootstrap/init.sh
+++ mongo/config_bootstrap/init.sh
@@ -15,7 +15,7 @@ if [[ $ordinal -ne 0 ]]; then
 fi
 
 # https://docs.mongodb.com/manual/tutorial/deploy-sharded-cluster-with-keyfile-access-control/
-mongod --configsvr --transitionToAuth --keyFile ${KEY_FILE} --replSet ${REPL_SET} --fork --logpath ${DB_ROOT}/init-admin.log --dbpath ${DB_ROOT} -vv
+gosu root mongod --configsvr --transitionToAuth --keyFile ${KEY_FILE} --replSet ${REPL_SET} --logpath ${DB_ROOT}/init-admin.log --dbpath ${DB_ROOT} -vv &
 if [ $? -ne 0 ]; then
     cat ${DB_ROOT}/init-admin.log
     exit 1
