ffc02203f0dea5f515ff6e33487b379afb9e7431
Mike Kaperys <mike@kaperys.io>
2019-12-18 10:28:33 +0000

    more experimenting

--- mongo/config_bootstrap/init.sh
+++ mongo/config_bootstrap/init.sh
@@ -5,8 +5,7 @@
 [ -z "$KEY_FILE" ] && echo "Error not set KEY_FILE" && exit 1
 [ -z "$ADMIN_PASSWORD" ] && echo "Error not set ADMIN_PASSWORD" && exit 1
 [ -z "$EXPORTER_PASSWORD" ] && echo "Error not set EXPORTER_PASSWORD" && exit 1
-[ -z "$APP_DB" ] && echo "Error not set APP_DB" && exit 1
-[ -z "$SHARD_NODES" ] && echo "Error not set SHARD_NODES" && exit 1
+[ -z "$DB_ROOT" ] && echo "Error not set DB_ROOT" && exit 1
 
 [[ `hostname` =~ -([0-9]+)$ ]] || exit 1
 ordinal=${BASH_REMATCH[1]}
@@ -17,7 +16,7 @@ if [[ $ordinal -ne 0 ]]; then
 fi
 
 # https://docs.mongodb.com/manual/tutorial/deploy-sharded-cluster-with-keyfile-access-control/
-gosu root mongod --configsvr --transitionToAuth --keyFile ${KEY_FILE} --replSet ${REPL_SET} --fork --logpath ${DB_ROOT}/init-admin.log --dbpath ${DB_ROOT} # 27019
+gosu root mongod --configsvr --transitionToAuth --keyFile ${KEY_FILE} --replSet ${REPL_SET} --fork --logpath ${DB_ROOT}/init-admin.log --dbpath ${DB_ROOT}
 if [ $? -ne 0 ]; then
     cat ${DB_ROOT}/init-admin.log
     exit 1
@@ -45,18 +44,6 @@ rs.initiate( {
 # Wait for MongoDB to become PRIMARY
 sleep 10
 
-# Shards cannot be added directly to MongoDB config servers, so we need to spin up a temporary mongos instance and add them
-echo "starting temporary mongos instance"
-set +e
-gosu root mongos --transitionToAuth --keyFile ${KEY_FILE} --fork --logpath ${DB_ROOT}/init-mongos.log --configdb ${REPL_SET}/localhost:27019 # 27018
-if [ $? -ne 0 ]; then
-    cat ${DB_ROOT}/init-mongos.log
-    exit 1
-fi
-
-set -e
-sleep 15
-
 echo "creating user ${ADMIN_USERNAME}"
 mongo --port 27019 --quiet --eval "
 db.getSiblingDB(\"admin\").createUser({
@@ -78,27 +65,6 @@ db.getSiblingDB(\"admin\").createUser({
         { role: \"read\", db: \"local\" }
     ]
 });"
-
-nodes=$(echo $SHARD_NODES | tr "," "\n")
-for node in $nodes
-do
-    echo "adding node shard node ${node}"
-    while true
-    do
-        script="mongo --port 27018 --quiet --eval 'sh.addShard(\"${node}\")'"
-        out=$(eval "$script")
-        echo $out
-        if [[ $out == *"shardAdded"* ]]; then
-            break
-        fi
-
-        echo "retrying ${node}"
-        sleep 5
-    done
-done
-
-echo "enabling sharing on ${APP_DB}"
-mongo --port 27018 --quiet --eval "sh.enableSharding(\"${APP_DB}\")"
 
 nodes=$(echo $REPLICATION_NODES | tr "," "\n")
 counter=1
