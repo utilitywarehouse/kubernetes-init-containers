e0e9e8c52b1b2605d2b533426bd95f8118683f1f
Mike Kaperys <mike@kaperys.io>
2019-12-19 10:23:36 +0000

    sharded mongo init containers  (#20)
    * config server init
    
    * experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting
    
    * more experimenting

--- mongo/config_bootstrap/init.sh
+++ mongo/config_bootstrap/init.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 [ -z "$REPLICATION_NODES" ] && echo "Error not set REPLICATION_NODES" && exit 1
-[ -z "$SHARD_NODES" ] && echo "Error not set SHARD_NODES" && exit 1
 [ -z "$REPL_SET" ] && echo "Error not set REPL_SET" && exit 1
+[ -z "$KEY_FILE" ] && echo "Error not set KEY_FILE" && exit 1
 [ -z "$ADMIN_PASSWORD" ] && echo "Error not set ADMIN_PASSWORD" && exit 1
 [ -z "$EXPORTER_PASSWORD" ] && echo "Error not set EXPORTER_PASSWORD" && exit 1
-[ -z "$MONGOLIZER_PASSWORD" ] && echo "Error not set MONGOLIZER_PASSWORD" && exit 1
-[ -z "$APP_PASSWORD" ] && echo "Error not set APP_PASSWORD" && exit 1
+[ -z "$DB_ROOT" ] && echo "Error not set DB_ROOT" && exit 1
 
 [[ `hostname` =~ -([0-9]+)$ ]] || exit 1
 ordinal=${BASH_REMATCH[1]}
 
-# Execute user creation and replication setup on the first node only
+# Only execute on the 0th node
 if [[ $ordinal -ne 0 ]]; then
     exit 0
 fi
 
-gosu root mongod --configsvr --transitionToAuth --keyFile ${KEY_FILE} --replSet ${REPL_SET} --fork --logpath ${DB_ROOT}/init-admin.log --port 27017 --dbpath ${DB_ROOT}
+# https://docs.mongodb.com/manual/tutorial/deploy-sharded-cluster-with-keyfile-access-control/
+gosu root mongod --configsvr --transitionToAuth --keyFile ${KEY_FILE} --replSet ${REPL_SET} --fork --logpath ${DB_ROOT}/init-admin.log --dbpath ${DB_ROOT}
 if [ $? -ne 0 ]; then
     cat ${DB_ROOT}/init-admin.log
     exit 1
@@ -25,51 +25,38 @@ fi
 set -e
 sleep 30
 
-# check if replication is initialized 
-res="$(mongo --quiet --eval='printjson(rs.status())')"
+# If the cluster has already been initialised, exit
+res="$(mongo --port 27019 --quiet --eval='printjson(rs.status())')"
 if [[ $res != *"NotYetInitialized"* ]]; then
     exit 0
 fi
 
 name=$(hostname -f)
 
-echo "starting bootstrap"
-mongo --quiet --eval "
+echo "initialising replicaset with 0th node"
+mongo --port 27019 --quiet --eval "
 rs.initiate( {
    _id : \"${REPL_SET}\",
-   members: [ { _id : 0, host : \"${name}\" } ]
+   configsvr: true,
+   members: [ { _id : 0, host : \"${name}:27019\" } ]
 })"
 
-# wait for mongodb to understand that it is master
+# Wait for MongoDB to become PRIMARY
 sleep 10
 
-# Seriously Mongo doesn't allow you to add shards to config server directly
-# you have to spin up a mongos router to do that
-echo "Starting mongos instance"
-
-set +e
-gosu root mongos --transitionToAuth --keyFile ${KEY_FILE} --fork --logpath ${DB_ROOT}/init-mongos.log --port 27018 --configdb ${REPL_SET}/localhost:27017
-if [ $? -ne 0 ]; then
-    cat ${DB_ROOT}/init-mongos.log
-    exit 1
-fi
-
-set -e
-sleep 15
-
 echo "creating user ${ADMIN_USERNAME}"
-mongo localhost:27018 --quiet --eval "
+mongo --port 27019 --quiet --eval "
 db.getSiblingDB(\"admin\").createUser({
   user: \"${ADMIN_USERNAME:?}\",
   pwd: \"${ADMIN_PASSWORD:?}\",
-  roles: [{ 
+  roles: [{
 	role: \"root\",
 	db: \"admin\"
   }]
-});
-" 
+});"
 
-mongo localhost:27018 --quiet --eval "
+echo "creating user ${EXPORTER_USERNAME}"
+mongo --port 27019 --quiet --eval "
 db.getSiblingDB(\"admin\").createUser({
     user: \"${EXPORTER_USERNAME:?}\",
     pwd: \"${EXPORTER_PASSWORD:?}\",
@@ -77,64 +64,30 @@ db.getSiblingDB(\"admin\").createUser({
         { role: \"clusterMonitor\", db: \"admin\" },
         { role: \"read\", db: \"local\" }
     ]
-});
-"
-
-mongo localhost:27018 --quiet --eval "
-db.getSiblingDB(\"admin\").createUser({
-    user: \"${MONGOLIZER_USERNAME:?}\",
-    pwd: \"${MONGOLIZER_PASSWORD:?}\",
-    roles: [{ role: \"backup\", db:\"admin\"}]
 });"
 
-mongo localhost:27018 --quiet --eval "
-db.getSiblingDB(\"${APP_DB:?}\").createUser({
-    user: \"${APP_USERNAME:?}\",
-    pwd: \"${APP_PASSWORD:?}\",
-    roles: [
-       { role: \"readWrite\", db: \"${APP_DB:?}\" }
-    ]
-});"
-
-echo "shard config init" 
-nodes=$(echo $SHARD_NODES | tr "," "\n")
-for node in $nodes
-do
-    while true
-    do
-        script="mongo localhost:27018 --quiet --eval 'sh.addShard(\"${node}\")'"
-        out=$(eval "$script")
-        echo $out
-        if [[ $out == *"shardAdded"* ]]; then
-            break
-        fi
-        echo "retrying ${node}"
-        sleep 5
-    done
-done
-
-mongo localhost:27018 --quiet --eval "sh.enableSharding(\"${APP_DB}\")"
-
-echo "rs config init ${name}"
 nodes=$(echo $REPLICATION_NODES | tr "," "\n")
 counter=1
 for node in $nodes
 do
-    echo "adding replica ${node}, ${counter}"
+    echo "adding replica ${counter} ${node}"
     while true
     do
-        script="mongo --quiet --eval 'rs.add({_id: ${counter}, host:\"${node}\", priority: 0.99})'"
+        script="mongo --port 27019 --quiet --eval 'rs.add({_id: ${counter}, host:\"${node}\", priority: 0.99})'"
         out=$(eval "$script")
         echo $out
         if [[ $out != *"NodeNotFound"* ]]; then
             break
         fi
+
         echo "retrying ${node}"
         sleep 5
     done
+
     counter=$[$counter +1]
 done
 
+echo "reconfiguring the replicaset with all replication nodes"
 NEWLINE=$'\n'
 script="cfg = rs.conf();$NEWLINE"
 script="$script cfg.members[0].priority = 1;$NEWLINE"
@@ -146,6 +99,7 @@ do
     counter=$[$counter +1]
 done 
 script="$script rs.reconfig(cfg);$NEWLINE"
-mongo --eval "$script"
+mongo --port 27019 --eval "$script"
 
-mongod --shutdown
+echo "initialisation complete"
+mongod --port 27019 --shutdown --dbpath ${DB_ROOT}
--- mongo/shard_bootstrap/init.sh
+++ mongo/shard_bootstrap/init.sh
@@ -2,19 +2,21 @@
 
 [ -z "$REPLICATION_NODES" ] && echo "Error not set REPLICATION_NODES" && exit 1
 [ -z "$REPL_SET" ] && echo "Error not set REPL_SET" && exit 1
+[ -z "$KEY_FILE" ] && echo "Error not set KEY_FILE" && exit 1
 [ -z "$ADMIN_PASSWORD" ] && echo "Error not set ADMIN_PASSWORD" && exit 1
 [ -z "$EXPORTER_PASSWORD" ] && echo "Error not set EXPORTER_PASSWORD" && exit 1
-[ -z "$MONGOLIZER_PASSWORD" ] && echo "Error not set MONGOLIZER_PASSWORD" && exit 1
+[ -z "$DB_ROOT" ] && echo "Error not set DB_ROOT" && exit 1
 
 [[ `hostname` =~ -([0-9]+)$ ]] || exit 1
 ordinal=${BASH_REMATCH[1]}
 
-# Execute user creation and replication setup on the first node only
+# Only execute on the 0th node
 if [[ $ordinal -ne 0 ]]; then
     exit 0
 fi
 
-gosu root mongod --shardsvr --transitionToAuth --keyFile ${KEY_FILE} --replSet ${REPL_SET} --fork --logpath ${DB_ROOT}/init-admin.log --port 27017 --dbpath ${DB_ROOT}
+# https://docs.mongodb.com/manual/tutorial/deploy-sharded-cluster-with-keyfile-access-control/
+gosu root mongod --shardsvr --transitionToAuth --keyFile ${KEY_FILE} --replSet ${REPL_SET} --fork --logpath ${DB_ROOT}/init-admin.log --dbpath ${DB_ROOT}
 if [ $? -ne 0 ]; then
     cat ${DB_ROOT}/init-admin.log
     exit 1
@@ -23,39 +25,37 @@ fi
 set -e
 sleep 30
 
-# check if replication is initialized 
-res="$(mongo --quiet --eval='printjson(rs.status())')"
+# If the cluster has already been initialised, exit
+res="$(mongo --port 27018 --quiet --eval='printjson(rs.status())')"
 if [[ $res != *"NotYetInitialized"* ]]; then
     exit 0
 fi
 
 name=$(hostname -f)
 
-echo "starting bootstrap"
-echo "${REPL_SET}"
-echo "${REPLICATION_NODES}"
-mongo --quiet --eval "
+echo "initialising replicaset with 0th node"
+mongo --port 27018 --quiet --eval "
 rs.initiate( {
    _id : \"${REPL_SET}\",
-   members: [ { _id : 0, host : \"${name}\" } ]
+   members: [ { _id : 0, host : \"${name}:27018\" } ]
 })"
 
-# wait for mongodb to understand that it is master
+# Wait for MongoDB to become PRIMARY
 sleep 10
 
 echo "creating user ${ADMIN_USERNAME}"
-mongo --quiet --eval "
+mongo --port 27018 --quiet --eval "
 db.getSiblingDB(\"admin\").createUser({
   user: \"${ADMIN_USERNAME:?}\",
   pwd: \"${ADMIN_PASSWORD:?}\",
-  roles: [{ 
+  roles: [{
 	role: \"root\",
 	db: \"admin\"
   }]
-});
-" 
+});"
 
-mongo --quiet --eval "
+echo "creating user ${EXPORTER_USERNAME}"
+mongo --port 27018 --quiet --eval "
 db.getSiblingDB(\"admin\").createUser({
     user: \"${EXPORTER_USERNAME:?}\",
     pwd: \"${EXPORTER_PASSWORD:?}\",
@@ -63,36 +63,30 @@ db.getSiblingDB(\"admin\").createUser({
         { role: \"clusterMonitor\", db: \"admin\" },
         { role: \"read\", db: \"local\" }
     ]
-});
-"
-
-mongo --quiet --eval "
-db.getSiblingDB(\"admin\").createUser({
-    user: \"${MONGOLIZER_USERNAME:?}\",
-    pwd: \"${MONGOLIZER_PASSWORD:?}\",
-    roles: [{ role: \"backup\", db:\"admin\"}]
 });"
-
 
 nodes=$(echo $REPLICATION_NODES | tr "," "\n")
 counter=1
 for node in $nodes
 do
-    echo "adding replica ${node}, ${counter}"
+    echo "adding replica ${counter} ${node}"
     while true
     do
-        script="mongo --quiet --eval 'rs.add({_id: ${counter}, host:\"${node}\", priority: 0.99})'"
+        script="mongo --port 27018 --quiet --eval 'rs.add({_id: ${counter}, host:\"${node}\", priority: 0.99})'"
         out=$(eval "$script")
         echo $out
         if [[ $out != *"NodeNotFound"* ]]; then
             break
         fi
+
         echo "retrying ${node}"
         sleep 5
     done
+
     counter=$[$counter +1]
 done
 
+echo "reconfiguring the replicaset with all replication nodes"
 NEWLINE=$'\n'
 script="cfg = rs.conf();$NEWLINE"
 script="$script cfg.members[0].priority = 1;$NEWLINE"
@@ -104,6 +98,7 @@ do
     counter=$[$counter +1]
 done 
 script="$script rs.reconfig(cfg);$NEWLINE"
-mongo --quiet --eval "$script"
+mongo --port 27018 --quiet --eval "$script"
 
-mongod --shutdown --dbpath ${DB_ROOT} 
+echo "initialisation complete"
+mongod --port 27018 --shutdown --dbpath ${DB_ROOT}
