359d14442b9c48cf6097acdd668c5a9e46773622
Mike Kaperys <mike@kaperys.io>
2021-08-27 12:46:08 +0100

    helps if you login

--- include.mk
+++ include.mk
@@ -15,7 +15,11 @@ build:
 	docker build -t $(DOCKER_REPOSITORY):$(GIT_HASH) .
 	docker tag $(DOCKER_REPOSITORY):$(GIT_HASH) $(DOCKER_REPOSITORY):latest
 
-ci-docker-build: 
+ci-docker-auth:
+	@echo "Logging in to $(DOCKER_REGISTRY) as $(DOCKER_ID)"
+	@docker login -u $(DOCKER_ID) -p $(DOCKER_PASSWORD) $(DOCKER_REGISTRY)
+
+ci-docker-build: ci-docker-auth
 	docker build -t $(DOCKER_REPOSITORY):$(CIRCLE_SHA1) .
 	docker tag $(DOCKER_REPOSITORY):$(CIRCLE_SHA1) $(DOCKER_REPOSITORY):latest
 	docker push $(DOCKER_REPOSITORY)
